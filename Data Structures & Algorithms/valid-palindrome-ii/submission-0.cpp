7c0d6763cfcbd7630474465c2ca70061eb1f5136
pirateninja2521 <66412287+pirateninja2521@users.noreply.github.com>
2026-08-29 12:07:38 +0800

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int l = 0, r = s.length()-1;
+
+        while (l < r) {
+            if (s[l] == s[r]) {
+                l++;
+                r--;
+            }
+            else {
+                return isPalindrome(s, l, r-1) || isPalindrome(s, l+1, r);
+            }
+        }
+        return true;
+    }
+
+private:
+    bool isPalindrome(string s, int l, int r) {
+        while (l < r) {
+            if (s[r] != s[l]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
