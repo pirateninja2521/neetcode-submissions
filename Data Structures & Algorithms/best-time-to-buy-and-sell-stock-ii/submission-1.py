9417468a671a597900f208fcaf06539138a5dea4
pirateninja2521 <66412287+pirateninja2521@users.noreply.github.com>
2026-08-31 12:06:03 +0800

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        hold = prices[0]
+        total = 0
+
+        for price in prices:
+            if price > hold:
+                total += price - hold
+            hold = price
+        
+        return total
