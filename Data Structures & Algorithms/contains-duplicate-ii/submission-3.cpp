92e69380e139c49d9548dced360dc43846c399eb
pirateninja2521 <66412287+pirateninja2521@users.noreply.github.com>
2026-09-20 22:40:36 +0800

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> seen;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if (seen.contains(nums[i])) {
+                return true;
+            }
+            seen.insert(nums[i]);
+
+            if (seen.size() > k) {
+                seen.erase(nums[i - k]);
+            }
+        }
+        return false;
+    }
+};
