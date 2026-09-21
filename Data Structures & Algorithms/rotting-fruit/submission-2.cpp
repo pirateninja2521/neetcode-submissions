fe8f0509e323fc9e81d87914d8fb100b56ec5efc
pirateninja2521 <66412287+pirateninja2521@users.noreply.github.com>
2026-09-21 20:58:06 +0800

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<int, int>> queue;
+        int freshcount = 0;
+        for(int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 2) {
+                    queue.push(make_pair(i, j));
+                }
+                else if (grid[i][j] == 1) freshcount++;
+            }
+        }
+
+        int minute = 0;
+
+        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        while (!queue.empty()) {
+            int size = queue.size();
+            while(size--) {
+                auto [x, y] = queue.front();
+                queue.pop();
+                for(auto [dx, dy] : directions) {
+                    if (x+dx >= 0 && x+dx < grid.size() && y+dy >= 0 && y+dy < grid[0].size() && grid[x+dx][y+dy] == 1) {
+                        queue.push(make_pair(x + dx, y + dy));
+                        grid[x + dx][y + dy] = 2;
+                        freshcount--;
+                    }
+                }
+            }
+            if (!queue.empty()) minute++;
+        };
+        return freshcount == 0 ? minute: -1;
+    }
+};
